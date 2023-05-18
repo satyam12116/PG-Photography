9ea75add00e88061572f6f4c6813c4c0b86fb608
satyam-pokee <satyam-pokee@gmail.com>
2023-05-18 09:20:28 +0530

    user/admin changes related to auth

--- src/app/user-auth/user-auth.component.ts
+++ src/app/user-auth/user-auth.component.ts
@@ -20,25 +20,6 @@ export class UserAuthComponent {
 
   }
   ngOnInit(): void {
-    const signupButton = document.getElementById('signup-button'),
-    loginButton = document.getElementById('login-button'),
-    userForms = document.getElementById('user_options-forms')
-
-/**
- * Add event listener to the "Sign Up" button
- */
-signupButton?.addEventListener('click', () => {
-  userForms?.classList.remove('bounceRight')
-  userForms?.classList.add('bounceLeft')
-}, false)
-
-/**
- * Add event listener to the "Login" button
- */
-loginButton?.addEventListener('click', () => {
-  userForms?.classList.remove('bounceLeft')
-  userForms?.classList.add('bounceRight')
-}, false)
     this.SignUpForm=this.fb.group({
       name:['',Validators.required],
       email:['',Validators.required],
